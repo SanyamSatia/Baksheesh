e390eff2427ac8dce8626b4b456a11c01c9d767b
7Aces <sanyam.satia@ymail.com>
2016-11-27 10:50:33 -0600

    Added functionality for the settings view controller

--- Baksheesh/ViewController.swift
+++ Baksheesh/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let TIME_INTERVAL = 600.0
+        
+        amountField.becomeFirstResponder()
+        
+        let defaults = UserDefaults.standard
+        
+        if defaults.object(forKey: "last_close_time") != nil && (defaults.object(forKey: "amount_save") == nil || defaults.bool(forKey: "amount_save") == true)
+        {
+            let last_close_time = defaults.object(forKey: "last_close_time") as! Date
+            let elapsed_time = Date().timeIntervalSince(last_close_time)
+            
+            if elapsed_time <= TIME_INTERVAL {
+                let amt = defaults.float(forKey: "old_amount")
+                amountField.text = String(amt)
+            }
+        }
+        
+        if defaults.object(forKey: "default_tip_percentage") != nil {
+            tipSlider.setValue(defaults.float(forKey: "default_tip_percentage"), animated: true)
+            calculateTip(tipSlider)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +74,10 @@ class ViewController: UIViewController {
         let amount = Float(amountField.text!) ?? 0.0
         let tip = amount * tipPercentage
         let total = amount + tip
+        
+        let defaults = UserDefaults.standard
+        defaults.set(tipPercentage, forKey: "current_tip_percentage")
+        defaults.set(amount, forKey: "old_amount")
         
         tipLabel.text = String(format: "$%.2lf", tip)
         totalLabel.text = String(format: "$%.2lf", total)
